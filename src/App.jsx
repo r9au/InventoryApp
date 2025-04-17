e2ffb0263d068d49885f474083984b8d8f702082
r9au <r9audurti@gmail.com>
2025-04-17 18:57:43 +0530

    app-error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import Reg from './components/Reg'
 import Setup from './components/Setup'
-import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Land from './components/Land'
 import { createBrowserRouter, RouterProvider, Routes } from 'react-router-dom'
 import Card from './components/Card'
 import Workspace from './components/Workspace'
+import Protectedroute from './components/Protectedroute.jsx'
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <Land />
+    },
+    {
+      path: '/Login',
+      element: <Login />
+    },
+    {
+      path: '/Signup',
+      element: <Reg />
+    },
+    {
+      path: '/Setup',
+      element: <Setup />
+    },
+    {
+      path:'/Reg',
+      element:<Reg/>
+    },
+    {
+      path:'/workspace/:id',
+      element:<Protectedroute><Workspace/></Protectedroute>
+    },
+    {
+       path:'/Card/:id',
+      element:<Card/>
+    }
+  ])
   return (
     <>
+    <RouterProvider router={routes}/>
     </>
   )
 }
